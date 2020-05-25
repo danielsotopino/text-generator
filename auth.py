7a49688680eaf44d3158fff59cf049d511ddfcf0
Daniel <daniel.soto@landscape.cl>
2020-05-25 01:07:02 +0000

    update .env keys

--- auth.py
+++ auth.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-consumer_key = os.getenv('twitter_text_generator_consumer_key')
-consumer_secret = os.getenv('twitter_text_generator_consumer_secret')
-access_token = os.getenv('twitter_text_generator_access_token')
-access_token_secret = os.getenv('twitter_text_generator_access_token_secret')
+consumer_key = os.getenv('consumer_key')
+consumer_secret = os.getenv('consumer_secret')
+access_token = os.getenv('access_token')
+access_token_secret = os.getenv('access_token_secret')
